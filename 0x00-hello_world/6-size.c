98563b68104a46c9db2f98169fc450a351a22b0a
MobuchiElly <ugwueleazer@gmail.com>
2023-02-16 13:04:43 -0800

    Introduction to C Programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,16 @@
  */
 int main(void)
 {
-	double d;
-	int i;
 	char c;
+	int i;
+	long int a;
+	long long int b;
+	float f;
 
-	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of a long int is: %lu.\n", (unsigned long)sizeof(a));
+	printf("The size of a long long int is: %lu.\n", (unsigned long)sizeof(b));
+	printf("The size of a float is: %lu.\n", (unsigned long)sizeof(f));
 	return (0);
 }
