92414c27b1e0e9bea12cf04a5db1b48c8825de07
MobuchiElly <ugwueleazer@gmail.com>
2023-02-17 11:09:35 -0800

    C- variables,for,while statements

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+i#include <stdio.h>
+/**
+ * main - Program that prints alphabet in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
