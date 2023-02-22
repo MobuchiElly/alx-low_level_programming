c191d5390bfffaba6e8ee0bb55b7fece5baa6727
MobuchiElly <ugwueleazer@gmail.com>
2023-02-21 19:28:30 -0800

    C Functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,14 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-			prod = n * m;
+			p = n * m;
 
-			if (prod <= 9)
+			if (p <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '0');
+				_putchar((p / 10) + '0');
 
-			_putchar((prod % 10) + '0');
+			_putchar((p % 10) + '0');
 		}
 		_putchar('\n');
 	}
