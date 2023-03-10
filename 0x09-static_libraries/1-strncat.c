fba7edd9bb42b41f21030802571bb4ae6c418545
MobuchiElly <ugwueleazer@gmail.com>
2023-03-10 09:23:14 -0800

    Creating C static libraries

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 
 char *_strncat(char *dest, char *src, int n)
 {
--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 
 unsigned int _strspn(char *s, char *accept)
 {
