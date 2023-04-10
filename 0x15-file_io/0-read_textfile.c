98639126ba12a33fbb7de4c391a971ee83915f45
MobuchiElly <ugwueleazer@gmail.com>
2023-04-10 13:21:05 -0700

    Function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,39 +8,41 @@
  *
  * Return: read and printed chars, or 0 if unsucessful
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	size_t count;
-	FILE *fp;
-	char *buff;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+	int fd;
 
 	if (filename == NULL)
+	return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	return (0);
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
 	{
-		return (0);
+	free(buffer);
+	close(fd);
+	return (0);
 	}
 
-	fp = fopen(filename, "r");
-	if (fp == NULL)
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		return (0);
+	free(buffer);
+	close(fd);
+	return (0);
 	}
 
-	buff = (char *)malloc(letters * sizeof(char));
-	if (buff == NULL)
-	{
-		fclose(fp);
-		return (0);
-	}
+	free(buffer);
+	close(fd);
 
-	count = fread(buff, sizeof(char), letters, fp);
-	if (count == 0 || ferror(fp))
-	{
-		fclose(fp);
-		free(buff);
-		return (0);
-	}
-	fclose(fp);
-	free(buff);
-	return (count);
+	return (bytes_written);
 }
